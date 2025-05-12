c204bc26d9fc77e962e50a64a9b7db85927c5c97
Vicmarcp <victormarcosp@gmail.com>
2025-05-12 15:50:37 +0200

    Update tareas.py

--- tareas.py
+++ tareas.py
@@ -1,10 +1,19 @@
+# Función agregar tarea
 def agregar_tarea(lista, tarea):
-lista.append(tarea)
-return lista
+    lista.append(tarea)
+    return lista
+
+
+# Función listar tarea
 def listar_tareas(lista):
-for i, t in enumerate(lista, 1):
-print(f"{i}. {t}")
+    for i, t in enumerate(lista, 1):
+        print(f"{i}. {t}")
+
+
+# Funnción eliminar tarea
 def eliminar_tarea(lista, indice):
-if 0 <= indice < len(lista):
-lista.pop(indice)
-return lista
+    if 0 <= indice < len(lista):
+        lista.pop(indice)
+        return lista
+    else:
+        print("Índice inválido")
